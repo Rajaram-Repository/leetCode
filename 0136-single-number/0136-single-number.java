769752d4c81c465ede4b38e653f3397375dfdc15
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-09-10 19:59:41 +0530

    Time: 1 ms (99.97%), Space: 44.9 MB (15.91%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if(nums.length==1)
-            return nums[0];
         int op = 0;
-        List <Integer> list = new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(list.contains(nums[i]))
-                op -=nums[i];
-            else{
-                list.add(nums[i]);  
-                op +=nums[i];
-            }
-        }
+        for(int i=0;i<nums.length;i++)
+            op ^=nums[i];
         return op;
     }
 }
