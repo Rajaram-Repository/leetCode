650905bcb570c84fe0d0bb8f81fb26d78308325d
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-09-11 14:34:53 +0530

    Time: 628 ms (6.18%), Space: 44 MB (74.40%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    List<List<Integer>> list = new ArrayList<>();
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        search(new ArrayList<>(),new ArrayList<>(),nums);
+        return list;
+    }
+    public void search(List l,List index ,int [] nums){
+        if(l.size()==nums.length){
+            if(list.contains(l))
+                return ;
+            list.add(new ArrayList<>(l));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(index.contains(i))
+                continue;
+            index.add(i);
+            l.add(nums[i]);
+            search(l,index,nums);
+            l.remove(l.size()-1);
+            index.remove(index.size()-1);
+        }
+    }
+}
