d941d06eb594e409da71cecc302560789f9e1f37
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-09-15 15:34:12 +0530

    Time: 7 ms (72.27%), Space: 55.1 MB (79.75%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -22,14 +22,10 @@ class Solution {
         return;
     }
     public boolean pal(String s){
-        int l=0;
-        int r=s.length()-1;
-        while(l<=r){
-            if(s.charAt(l)!=s.charAt(r))
+        int l=0 , r=s.length()-1;
+        while(l<=r)
+            if(s.charAt(l++)!=s.charAt(r--))
                 return false;
-            l++;
-            r--;
-        }
         return true;
     }
 }
