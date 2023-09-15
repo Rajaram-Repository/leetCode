8ab82558f38aba30dba98f74e504a5b6e9a7f40c
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-09-15 15:52:28 +0530

    Time: 46 ms (5.72%), Space: 44.6 MB (20.93%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int max = heights[0] ;
+        int index = 0 ;
+        for(int i=0;i<names.length;i++){
+            max = heights[i];
+            index = i;
+            for(int j=i;j<names.length;j++){
+                if(max<heights[j]){
+                    max=heights[j];
+                    index = j ;
+                }
+            }
+            System.out.println(index);
+            if(index == i)
+                continue ;
+            int temp = heights[index];
+            heights[index] = heights[i];
+            heights[i] = temp;
+            String temps = names[index];
+            names[index] = names[i];
+            names[i] = temps;
+        }
+        return names;
+    }
+}
