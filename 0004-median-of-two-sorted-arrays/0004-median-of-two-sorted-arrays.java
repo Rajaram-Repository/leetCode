5a7a96e0db04bde90d362d1a3bcebda2a1ec94a0
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-09-21 13:10:36 +0530

    Time: 1 ms (100.00%), Space: 44.7 MB (53.18%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,29 +1,27 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int sum = nums1.length+nums2.length;
-        boolean even = false;
-        if(sum%2==0)
-            even = true;
-        sum /=2;
-        int n1 =0,n2=0;
-        int arr [] = new int[sum+1];
-        int i=0;
-        for(i=0;i<=sum&&n1<nums1.length && n2<nums2.length;i++)
+        int total = nums1.length+nums2.length;
+        int mid = total/2;
+        int arr [] = new int [mid+1];
+        int i = 0 , n1 = 0 , n2 = 0;
+        
+        for(i=0; i<=mid && n1<nums1.length && n2<nums2.length; i++)
             if(nums1[n1]<nums2[n2])
                 arr[i]=nums1[n1++] ;
             else 
                 arr[i]=nums2[n2++] ;
+        
         if(n1<nums1.length)
             mtd (arr,i,nums1,n1);
         else
             mtd (arr,i,nums2,n2);
-        if(even)
-            return ((double)arr[sum-1] +(double)arr[sum]) /2;
-        return (double)arr[sum];
+        
+        if(total%2==0)
+            return ((double)arr[mid-1] +(double)arr[mid]) /2;
+        return (double)arr[mid];
             
     }
     public void mtd(int []num,int pos,int [] nums,int n){
-        for(int i=pos;i<num.length;i++)
-            num[i]=nums[n++];
+        for(int i=pos;i<num.length;i++) num[i]=nums[n++];
     }
 }
