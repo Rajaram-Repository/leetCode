f09c9a040e1baf77dfc5969dc38677acfc452cae
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-10-03 14:57:34 +0530

    Time: 1 ms (86.48%), Space: 39.8 MB (60.02%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -3,7 +3,7 @@ class Solution {
         int c =0;
         for(int i=0;i<nums.length;i++)
             for(int j=i+1;j<nums.length;j++)
-                if(nums[i]==nums[j] && i<j)
+                if(nums[i]==nums[j])
                     c++;
         return c;
     }
