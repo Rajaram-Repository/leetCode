69f299552d2efd97ad0693305e6956ef37a6c24f
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-10-04 15:18:33 +0530

    Time: 30 ms (39.67%), Space: 54.4 MB (15.09%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,39 +1,17 @@
 class MyHashMap {
-    int map [][] ;
+    int[] data;
     public MyHashMap() {
-        map = new int[10][2];
+        data = new int[1000001];
+        Arrays.fill(data, -1);
     }
-    
-    public void put(int key, int value) {
-        if(map.length<=key)
-            map = resize(key);
-        map[key][0]=key==0?-1:key;
-        map[key][1]=value;
-    }
-    public int[][] resize (int key){
-        int arr[][] = new int[key+1][2];
-        for(int i=0;i<map.length;i++){
-            arr[i][0]=map[i][0];
-            arr[i][1]=map[i][1];
-        }
-        return arr;
+    public void put(int key, int val) {
+        data[key] = val;
     }
     public int get(int key) {
-        try{
-            return map[key][0]==0?-1: map[key][1];
-        }
-        catch(Exception e ){
-            return -1;    
-        }
+        return data[key];
     }
-    
     public void remove(int key) {
-        try{
-            map[key][0]=0;
-        }
-        catch(Exception e ){
-            
-        }
+        data[key] = -1;
     }
 }
 
