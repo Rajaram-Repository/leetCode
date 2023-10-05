70c4094a7c4f83fc09ff1c08c6ef96a02beca9d1
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-10-05 14:35:34 +0530

    Time: 17 ms (5.91%), Space: 46 MB (89.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -3,14 +3,13 @@ class Solution {
         int val =nums.length/3;
         List<Integer> list = new ArrayList<>();
         Map <Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++)
+        for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i]))
                 map.put(nums[i],map.get(nums[i])+1);
             else
                 map.put(nums[i],1);
-        for(Integer x : map.keySet()){
-            if(map.get(x)>val)
-                list.add(x);
+            if(map.get(nums[i])>val && !list.contains(nums[i]))
+                list.add(nums[i]);
         }
         return list;
     }
