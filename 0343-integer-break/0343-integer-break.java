dcaf41584a327285664b633a1ec705df67c26f83
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-10-06 14:47:33 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (85.29%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int integerBreak(int n) {
+        if (n <= 3) 
+            return n - 1;
+        int result = 1;
+        while (n > 4) {
+            result *= 3;
+            n -= 3;
+        }
+        result *= n;
+        return result;
+    }
+}
