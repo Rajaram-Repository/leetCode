00dbdcb6bbc583834796221686ec00a3d4ce1268
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-10-09 12:27:19 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (82.88%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,31 +1,27 @@
 class Solution {
-    int [] ans ;
     public int[] searchRange(int[] nums, int target) {
-        ans = new int [2];
-        ans[0]=ans[1]=-1;
-        int l=0;
-        int r=nums.length-1;
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(nums[mid]==target){
-                ans[0]=ans[1]=mid;
-                for(int i=mid;i>=0;i--)
-                    if(nums[i]==target)
-                        ans[0]=i;
-                    else
-                        break;
-                for(int i=mid;i<nums.length;i++)
-                    if(nums[i]==target)
-                        ans[1]=i;
-                    else
-                        break;
-                return ans;
+        int left = 0, right = nums.length - 1;
+        int first = -1, last = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                first = mid;
+                last = mid;
+                while (first > 0 && nums[first - 1] == target) {
+                    first--;
+                }
+                while (last < nums.length - 1 && nums[last + 1] == target) {
+                    last++;
+                }
+                break;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-            else if(nums[mid]<target)
-                l=mid+1;
-            else
-                r=mid-1;
         }
-        return ans;
+
+        return new int[]{first, last};
     }
 }
