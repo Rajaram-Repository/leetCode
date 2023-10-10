47481e2bde2abe15e7c640706dee48b57ea8e20e
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-10-10 16:52:27 +0530

    Time: 37 ms (90.20%), Space: 57.3 MB (63.73%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -3,16 +3,12 @@ class Solution {
         int n = nums.length;
         Arrays.sort(nums); 
         int m = 1;
-        for (int i = 1; i < n; i++) 
-            if (nums[i] != nums[i - 1]) 
-                nums[m++] = nums[i];
+        for (int i = 1; i < n; i++) if (nums[i] != nums[i - 1]) nums[m++] = nums[i];
         int ans = n;
         for (int i = 0, j = 0; i < m; i++) {
-            while (j < m && nums[j] - nums[i] <= n - 1) 
-                j++;
+            while (j < m && nums[j] - nums[i] <= n - 1) j++;
             ans = Math.min(ans, n - (j - i));
-            if(ans < i || ans==0 )
-                break;
+            if(ans < i || ans==0 ) break;
         }
         return ans;
     }
