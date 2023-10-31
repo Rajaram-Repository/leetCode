0dd8649c8b7b8adc05e236d70d834f66c84f425b
RajaSparrow <117366324+RajaSparrow@users.noreply.github.com>
2023-10-31 13:36:22 +0530

    Time: 162 ms (62.09%), Space: 74.5 MB (14.28%) - LeetHub

---
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} pref
+ * @return {number[]}
+ */
+var findArray = function(pref) {
+    const n = pref.length;
+    const result = new Array(n);
+    result[0] = pref[0];
+
+    for (let i = 1; i < n; i++) {
+        result[i] = pref[i] ^ pref[i - 1];
+    }
+
+    return result;
+};
