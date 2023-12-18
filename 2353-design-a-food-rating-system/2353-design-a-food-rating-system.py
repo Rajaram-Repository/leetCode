f60985ef4230b789bfa4623a20771a75c80bdb4c
Rajaram <117366324+Rajaram-Repository@users.noreply.github.com>
2023-12-19 00:40:06 +0530

    Time: 731 ms (81.66%), Space: 47.7 MB (57.40%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+import heapq
+class FoodRatings:
+
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        self.food_to_cuisine = {}
+        self.food_to_rating = defaultdict(int)
+
+        self.cuisine_to_heap = defaultdict(list)
+        for i in range(len(foods)):
+            self.food_to_cuisine[foods[i]] = cuisines[i]
+            self.food_to_rating[foods[i]] = -ratings[i]
+
+            heapq.heappush(self.cuisine_to_heap[cuisines[i]], (-ratings[i], foods[i]))
+            
+        
+
+    def changeRating(self, food, newRating):
+        """
+        :type food: str
+        :type newRating: int
+        :rtype: None
+        """
+        cuisine = self.food_to_cuisine[food]
+        heapq.heappush(self.cuisine_to_heap[cuisine], (-newRating, food))
+        self.food_to_rating[food] = -newRating
+
+    def highestRated(self, cuisine):
+        """
+        :type cuisine: str
+        :rtype: str
+        """
+        smallest_lexico = None
+        while len(self.cuisine_to_heap[cuisine]) > 0:
+            curr = self.cuisine_to_heap[cuisine][0]
+            if curr[0] != self.food_to_rating[curr[1]]:
+                # delete old data
+                heapq.heappop(self.cuisine_to_heap[cuisine])
+                continue
+            smallest_lexico = curr[1]
+            break
+        return smallest_lexico
+
+                
+            
+            
+        
+
+
+# Your FoodRatings object will be instantiated and called as such:
+# obj = FoodRatings(foods, cuisines, ratings)
+# obj.changeRating(food,newRating)
+# param_2 = obj.highestRated(cuisine)
