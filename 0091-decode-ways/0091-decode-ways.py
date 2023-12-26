c9a68db815568a29abe1f32613b32037d5ea780e
Rajaram <117366324+Rajaram-Repository@users.noreply.github.com>
2023-12-26 23:02:42 +0530

    Time: 46 ms (17.67%), Space: 17.2 MB (5.04%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        my_list = ["00","30","40","50","60","70","80","90"]
+        if any(substring in s for substring in my_list):
+            return 0
+        l = len(s) + 1
+        dp = [0 ]*l 
+
+        dp[0] = 1 
+        dp[1] = 0 if s[0] == "0" else 1 
+
+        for i in range(2,l): 
+            # One step jump
+            a = s[i-1:i]
+            if 0 < int(s[i-1:i]) <= 9:
+                dp[i] += dp[i - 1]
+            # Two step jump
+            b = s[i-2:i]
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i - 2]
+        return dp[len(s)]
+        
+                
