70f23f6c5c264dc0668d628010ddb30793830af5
Rajaram <117366324+Rajaram-Repository@users.noreply.github.com>
2023-12-26 23:02:52 +0530

    Time: 36 ms (77.77%), Space: 17.4 MB (5.04%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,8 +1,5 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        my_list = ["00","30","40","50","60","70","80","90"]
-        if any(substring in s for substring in my_list):
-            return 0
         l = len(s) + 1
         dp = [0 ]*l 
 
