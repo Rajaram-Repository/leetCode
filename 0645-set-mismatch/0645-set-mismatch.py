26f55f01cee63aad7b9667150c0f3ccee8eb95c3
Rajaram <117366324+Rajaram-Repository@users.noreply.github.com>
2024-01-22 18:35:29 +0530

    Time: 136 ms (99.83%), Space: 18.6 MB (51.72%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -2,14 +2,5 @@ from collections import Counter
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         l =len(nums)
-        arr = [True] * (l+1)
-        t = 0
-        val = 0
-        for i in nums:
-            if arr[i]:
-                val+=i
-                t+=i
-            else:
-                t+=i
-            arr[i]=False
-        return [t-val, l*(l+1)//2-val]
+        val =sum(set(nums))
+        return [sum(nums)-val, l*(l+1)//2-val]
