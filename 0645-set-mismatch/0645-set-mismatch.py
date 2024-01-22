6946ceb0ec03aeb32b3d0022ff135480f68c7738
Rajaram <117366324+Rajaram-Repository@users.noreply.github.com>
2024-01-22 17:52:01 +0530

    Time: 152 ms (92.33%), Space: 19 MB (34.66%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -2,15 +2,14 @@ from collections import Counter
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         seen = set()
+
         for num in nums:
             if num in seen:
                 f=num
                 break
             else:
                 seen.add(num)
-        
-        seen.update(nums)
+        counter = Counter(nums)
         for i in range(1,len(nums)+1):
-            if i not in nums:
+            if i not in counter:
                 return [f,i]
-        
