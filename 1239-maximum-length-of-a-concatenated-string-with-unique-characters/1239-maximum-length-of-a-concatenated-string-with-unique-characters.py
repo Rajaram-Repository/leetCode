d58d3c72a6d2fcfd35a71df273fc50ba752e6e38
Rajaram <117366324+Rajaram-Repository@users.noreply.github.com>
2024-01-23 15:39:41 +0530

    Time: 62 ms (95.92%), Space: 16.7 MB (74.18%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -7,8 +7,9 @@ class Solution:
     def dfs(self, arr, path, idx, result):
         if result[0]<len(path) and len(path) == len(set(path)):
             result[0] =len(path)
-        if idx == len(arr):
+        if idx == len(arr) or len(path) != len(set(path)):
             return
 
         for i in range(idx, len(arr)):
             self.dfs(arr, path + arr[i], i + 1, result)
+
