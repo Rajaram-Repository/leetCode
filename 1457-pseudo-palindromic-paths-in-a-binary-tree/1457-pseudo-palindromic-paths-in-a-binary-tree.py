9df2b93342786f09c7375824c55d4706ec1f9df0
Rajaram <117366324+Rajaram-Repository@users.noreply.github.com>
2024-01-24 16:11:54 +0530

    Time: 500 ms (69.78%), Space: 61.3 MB (53.78%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -13,7 +13,7 @@ class Solution:
     def res(self,root,new_l):
         if not root:
             return
-        if root.val in new_l and root.val != None:
+        if root.val in new_l:
             new_l.remove(root.val)
         else:
             new_l.add(root.val)
