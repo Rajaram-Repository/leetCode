0039bb51e20a2a3e81ab5b9be34b402a7c16de16
Rajaram <117366324+Rajaram-Repository@users.noreply.github.com>
2024-01-30 22:35:29 +0530

    Time: 75 ms (31.36%), Space: 17 MB (71.20%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -4,15 +4,13 @@ class Solution:
         stack = []
         ops = {
             "+": lambda a, b: a + b,
-            "-": lambda a, b: a - b,
-            "/": lambda a, b: int(a / b),
+            "-": lambda a, b: b-a,
+            "/": lambda a, b: int(b / a),
             "*": lambda a, b: a * b
         }
         for i in tokens :
             if i in "+-*/":
-                b=int(stack.pop())
-                a=int(stack.pop())
-                stack.append(str(ops[i](a,b)))
+                stack.append(str(ops[i](int(stack.pop()),int(stack.pop()))))
             else:
                 stack.append(i)
         return int(stack[0])
