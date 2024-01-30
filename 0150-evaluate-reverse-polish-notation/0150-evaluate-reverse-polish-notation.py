55ecbdc5b2581762c080ee2c1728f74fa54753b5
Rajaram <117366324+Rajaram-Repository@users.noreply.github.com>
2024-01-30 15:43:05 +0530

    Time: 77 ms (26.34%), Space: 17 MB (71.20%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,6 +1,7 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         i = 0
+        
         while len(tokens)!=1 :
             if tokens[i] in "+-*/":
                 tokens[i] = self.cal(tokens[i-2],tokens[i-1],tokens[i])
@@ -10,14 +11,14 @@ class Solution:
             else:
                 i+=1
         return int(tokens[0])
-    def cal(self,a,b,c):
+    def cal(self, a, b, c):
         a = int(a)
         b = int(b)
-        if c=="+":
-            return str(a+b)
-        elif c=="-":
-            return str(a-b)
-        elif c=="*":
-            return str(a*b)
-        return str(int(a/b))
-        
+        ops = {
+            "+": lambda a, b: a + b,
+            "-": lambda a, b: a - b,
+            "/": lambda a, b: int(a / b),
+            "*": lambda a, b: a * b
+        }
+        result = ops[c](a,b)
+        return str(result)
