c4cc1813c80ed47ba162759c8936275caeaa8990
Rajaram <117366324+Rajaram-Repository@users.noreply.github.com>
2024-02-11 13:37:31 +0530

    Time: 1 ms (79.04%), Space: 45.5 MB (45.23%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -12,21 +12,22 @@ class Solution {
         }
         int c =0;
         while(c<row.size()){
+            int i=0;
             if(matrix[0].length<matrix.length){
-                for(int i=0;i<matrix.length;i++){
+                for(i=0;i<matrix[0].length;i++){
                     matrix[i][col.get(c)]=0;                    
-                    if(i<matrix[0].length){
-                        matrix[row.get(c)][i]=0;
-                    }
+                    matrix[row.get(c)][i]=0;
                 }
+                while(i<matrix.length)
+                    matrix[i++][col.get(c)]=0;
             }
             else{
-                for(int i=0;i<matrix[0].length;i++){
+                for(i=0;i<matrix.length;i++){
                     matrix[row.get(c)][i]=0;
-                    if(i<matrix.length){
-                        matrix[i][col.get(c)]=0;
-                    }
+                    matrix[i][col.get(c)]=0;    
                 }
+                while(i<matrix[0].length)
+                    matrix[row.get(c)][i++]=0;
             }
             c++;
         }
