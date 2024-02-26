8cbba57a6e758069412a8b1bffc25b4c0c48820f
Rajaram <117366324+Rajaram-Repository@users.noreply.github.com>
2024-02-26 15:18:37 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (7.57%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -21,16 +21,8 @@ class Solution {
     }
     public void find(TreeNode p, TreeNode q){
         if(p==null && q==null)
-            return ;
-        if(p==null && q!=null){
-            flag=false;
             return;
-        }
-        if(q==null && p!=null){
-            flag=false;
-            return;
-        }
-        if(p.val!=q.val){
+        if(p==null && q!=null || (q==null && p!=null) || (p.val!=q.val) ){
             flag=false;
             return;
         }
